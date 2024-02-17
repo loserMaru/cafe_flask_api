c2baf1eef28f51791a593b50c3bab18de141b83e
Maru <osmanov.m.i.20@gmail.com>
2024-02-17 23:53:04 +0300

    db fix for flask_sqlalchemy

--- app/database/db.py
+++ app/database/db.py
@@ -1,11 +1,3 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
+from flask_sqlalchemy import SQLAlchemy
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+db = SQLAlchemy()
