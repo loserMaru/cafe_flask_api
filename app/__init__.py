8e5409f0272660770c5ed912725b4d9bd660e259
Maru <osmanov.m.i.20@gmail.com>
2024-02-17 23:53:43 +0300

    Global api variable

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,29 @@
 from flask import Flask
 from flask_restx import Api
-from app.api.resources.user import HelloWorld as UserResource
+
+from app.api.resources.user import UserList, User
+from app.database import db
+
+# Инициализация расширения Flask-RESTX
+api = Api(version='1.0', title='Cafe Flask Api', description='Swagger documentation for cafe app',
+          doc='/docs/', authorizations=
+          {'jwt': {
+              'type': 'apiKey',
+              'in': 'header',
+              'name': 'Authorization',
+              'description': 'JWT authorization, e.g. "token"'
+          }
+          })
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-
-    # Инициализация расширения Flask-RESTX
-    api = Api(app, version='1.0', title='Cafe Flask Api', description='Swagger documentation for cafe app',
-              doc='/docs/')
+    db.init_app(app)
+    api.init_app(app)
 
     # Регистрация ресурсов API
-    api.add_resource(UserResource, '/hello')
+    api.add_resource(UserList, '/users')
+    api.add_resource(User, '/users/<int:user_id>')
 
     return app
