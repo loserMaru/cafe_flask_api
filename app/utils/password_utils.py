af1f04de019b3968c2c62247176a2bb490d6e243
Maru <osmanov.m.i.20@gmail.com>
2024-02-19 00:20:43 +0300

    Bcrypt initialization

---
+++ app/utils/password_utils.py
@@ -0,0 +1,10 @@
+from app.utils import bcrypt
+
+
+def hash_password(password):
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    return hashed_password
+
+
+def check_password(password, hashed_password):
+    return bcrypt.check_password_hash(hashed_password, password)
