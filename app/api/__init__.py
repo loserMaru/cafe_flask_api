ffc305c4e8b1d19d9e0a2aa7bafc69e0e698cf06
Maru <osmanov.m.i.20@gmail.com>
2024-02-19 03:25:59 +0300

    Authorization description change

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,13 +2,14 @@ from flask_restx import Api
 
 api = Api(version='1.0', title='Cafe Flask Api', description='Swagger documentation for cafe app',
           doc='/docs/', authorizations=
-          {'jwt': {
-              'type': 'apiKey',
-              'in': 'header',
-              'name': 'Authorization',
-              'description': 'JWT authorization, e.g. "token"'
-          }
-          })
+          {'jwt':
+              {
+                  'type': 'apiKey',
+                  'in': 'header',
+                  'name': 'Authorization',
+                  'description': "Type in the *'Value'* input box below: **'Bearer &lt;JWT&gt;'**",
+              }}
+          )
 
 # Очистка default namespace
 api.namespaces.clear()
@@ -17,4 +18,3 @@ api.namespaces.clear()
 api_namespace = api.namespace('api', description='API operations', doc='')
 login_namespace = api.namespace('Login', description='JWT-token receiving', path='/login')
 user_namespace = api.namespace('Users', description='Users operations', path='/user', doc='')
-
