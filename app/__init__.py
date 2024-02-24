d32ca3c4d1f23c2fb4eb924e3f94b0cfaa9b1959
Maru <osmanov.m.i.20@gmail.com>
2024-02-24 18:50:49 +0300

    2 new resources registration coffee and cafe

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 from app.api import api
-from app.api.resources.login import UserLogin, RefreshToken
-from app.api.resources.user import UserList, User
-from app.database import db
 from app.utils import jwt
+from app.database import db
+from app.api.resources.user import UserList, User
+from app.api.resources.cafe import CafeList, Cafe
+from app.api.resources.coffee import CoffeeList, Coffee
+from app.api.resources.login import UserLogin, RefreshToken
 
 
 # Инициализация расширения Flask-RESTX
@@ -23,5 +25,13 @@ def create_app():
     # Регистрация ресурсов API
     api.add_resource(UserList, '/users')
     api.add_resource(User, '/users/<int:user_id>')
+
+    # Регистрация ресурсов API для кофе
+    api.add_resource(CoffeeList, '/coffee')
+    api.add_resource(Coffee, '/coffee/<int:coffee_id>')
+
+    # Регистрация ресурсов API для кафе
+    api.add_resource(CafeList, '/cafe')
+    api.add_resource(Cafe, '/cafe/<int:cafe_id>')
 
     return app
