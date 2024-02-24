fc0b5e6b8769ef36082f086f4b4b23800a94ef8c
Maru <osmanov.m.i.20@gmail.com>
2024-02-24 21:40:12 +0300

    Swap db to MySQL

--- alembic/versions/754ff5c71925_swap_to_mysql.py
+++ alembic/versions/754ff5c71925_swap_to_mysql.py
@@ -1,8 +1,8 @@
-"""Init migration
+"""Swap to MySQL
 
-Revision ID: 5cfdd730795f
+Revision ID: 754ff5c71925
 Revises: 
-Create Date: 2024-02-17 22:30:59.474971
+Create Date: 2024-02-24 21:19:37.110654
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '5cfdd730795f'
+revision: str = '754ff5c71925'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
