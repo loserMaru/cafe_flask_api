f59736a5e6b6744a818d58eefa27ccfda3242a18
Maru <osmanov.m.i.20@gmail.com>
2024-02-26 00:34:59 +0300

    Order resource registered

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,9 @@ from flask_jwt_extended.exceptions import NoAuthorizationError
 from app.api import api
 from app.utils import jwt
 from app.database import db
-from app.api.resources.user import UserList, User
 from app.api.resources.cafe import CafeList, Cafe
+from app.api.resources.user import UserList, User
+from app.api.resources.order import OrderList, Order
 from app.api.resources.coffee import CoffeeList, Coffee
 from app.api.resources.login import UserLogin, RefreshToken
 
@@ -15,9 +16,9 @@ from app.api.resources.login import UserLogin, RefreshToken
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    db.init_app(app)
     api.init_app(app)
     jwt.init_app(app)
-    db.init_app(app)
 
     # Регистрация ресурсов API
     api.add_resource(UserLogin, '/login')
@@ -34,6 +35,10 @@ def create_app():
     # Регистрация ресурсов API для кафе
     api.add_resource(CafeList, '/cafe')
     api.add_resource(Cafe, '/cafe/<int:cafe_id>')
+
+    # Регистрация ресурсов API для заказов
+    api.add_resource(OrderList, '/orders')
+    api.add_resource(Order, '/orders/<int:order_id>')
 
     @app.errorhandler(NoAuthorizationError)
     def handle_missing_authorization_header_error(error):
