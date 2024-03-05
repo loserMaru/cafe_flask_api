bf2e4acd60553a0ec6b0856fc6d6a09005230615
Marlen Osmanov <91006226+loserMaru@users.noreply.github.com>
2024-03-05 12:53:32 +0300

    Subscription put and delete requests

--- app/api/resources/subscription.py
+++ app/api/resources/subscription.py
@@ -1,14 +1,15 @@
 import datetime
+import sqlalchemy
 
 from flask import request
-from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restx import Resource
 from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
+from app.database import db
 from app.api import subscription_namespace
 from app.api.models import subscription_model
 from app.api.resources.orm_models import SubscriptionModel
-from app.database import db
 
 
 @subscription_namespace.route('/')
@@ -33,6 +34,11 @@ class SubscriptionList(Resource):
         current_user = get_jwt_identity()
         user_id = current_user.get('id')
 
+        # Проверка наличия подписки у данного пользователя
+        existing_subscription = SubscriptionModel.query.filter_by(user_id=user_id).first()
+        if existing_subscription:
+            return {'msg': 'У вас уже есть активная подписка'}
+
         start_date = datetime.date.today()
         data['start_date'] = start_date
 
@@ -48,3 +54,36 @@ class SubscriptionList(Resource):
             db.session.rollback()
             subscription_namespace.abort(400, "Something wrong")
         return subscription
+
+
+@subscription_namespace.route('/<int:subscription_id>')
+class Subscription(Resource):
+    @subscription_namespace.doc(security='jwt')
+    @jwt_required()
+    @subscription_namespace.expect(subscription_model)
+    @subscription_namespace.marshal_with(subscription_model)
+    def put(self, subscription_id):
+        """Обновить информацию о подписке"""
+        subscription = SubscriptionModel.query.get(subscription_id)
+        if not subscription:
+            subscription_namespace.abort(404, "Subscription not found")
+        data = request.json
+        for key, value in data.items():
+            setattr(subscription, key, value)
+        db.session.commit()
+        return subscription
+
+    @subscription_namespace.doc(security='jwt')
+    @jwt_required()
+    def delete(self, subscription_id):
+        """Удаление подписки"""
+        subscription = SubscriptionModel.query.get(subscription_id)
+        if not subscription:
+            subscription_namespace.abort(404, message='Подписка с id {} не найдена'.format(subscription_id))
+        try:
+            db.session.delete(subscription)
+            db.session.commit()
+            return {'msg': 'Подписка удалена'}, 200
+        except sqlalchemy.exc.IntegrityError as e:
+            db.session.rollback()
+            return {'msg': 'Ошибка. У подписки есть внешние ключи'}, 200
