81ecaeea13ca305f8de0f13f931fca1580543182
Marlen Osmanov <91006226+loserMaru@users.noreply.github.com>
2024-04-13 23:21:57 +0300

    New post request

--- app/api/resources/order.py
+++ app/api/resources/order.py
@@ -1,13 +1,17 @@
+from datetime import datetime
+
+import pytz
 import sqlalchemy
 from flask import request
 from flask_restx import Resource
+from sqlalchemy import and_
 from sqlalchemy.exc import IntegrityError
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from app.database import db
 from app.api import order_namespace
 from app.api.models import order_model, order_post_model
-from app.api.resources.orm_models import OrderModel
+from app.api.resources.orm_models import OrderModel, CoffeeModel, CafeModel, SubscriptionModel
 from app.api.marshmallow.schemas import order_schema
 
 
@@ -36,20 +40,69 @@ class OrderList(Resource):
 
     @order_namespace.doc(security='jwt')
     @jwt_required()
-    @order_namespace.expect(order_post_model)
+    @order_namespace.expect(order_model)
     @order_namespace.marshal_with(order_model)
     def post(self):
         """Создание нового заказа"""
         data = request.json
         current_user = get_jwt_identity()
         user_id = current_user.get('id')
-        order = OrderModel(user_id=user_id, **data)  # Вставляем user_id из JWT токена
-        db.session.add(order)
+        coffee_data = data.pop('coffee', None)  # Извлекаем данные о кофе из запроса
+
+        # Получаем текущее время в часовом поясе UTC+3
+        timezone = pytz.timezone('Europe/Moscow')
+        current_time = datetime.now(timezone)
+
+        order = OrderModel(user_id=user_id, time=current_time, **data)  # Вставляем user_id из JWT токена
+
+        user_subscription = SubscriptionModel.query.filter_by(user_id=user_id).first()
+        if not user_subscription or user_subscription.quantity == 0:
+            order_namespace.abort(400, "Ошибка. Вы не можете совершать заказы. Пожалуйста, продлите подписку.")
+            return
+
+        cafe = CafeModel.query.get(data['cafe_id'])
+        if not cafe:
+            order_namespace.abort(404, "Кафе с указанным идентификатором не найдено")
+            return
+
+        if coffee_data:  # Если есть данные о кофе
+            if data['cafe_id'] != coffee_data['cafe_id']:
+                order_namespace.abort(400, "Ошибка. В данном кафе такого кофе не существует")
+                return
+
+            # Проверяем существование кофе в базе данных по названию и идентификатору кафе
+            existing_coffee = CoffeeModel.query.filter(
+                and_(CoffeeModel.name == coffee_data['name'], CoffeeModel.cafe_id == coffee_data['cafe_id'])).first()
+
+            if existing_coffee:  # Если кофе уже существует
+                coffee = existing_coffee
+            else:  # Если кофе не существует
+                order_namespace.abort(404, "Кофе с указанным названием и идентификатором кафе не найден")
+                return  # Досрочный выход из функции, чтобы избежать создания заказа
+
+            try:
+                db.session.commit()  # Фиксируем изменения, чтобы получить ID кофе
+            except IntegrityError:
+                db.session.rollback()
+                order_namespace.abort(400, "Ошибка. Невозможно создать заказ")
+
+            order.coffee_id = coffee.id  # Привязываем кофе к заказу
+
+        db.session.add(order)  # Добавляем заказ в сессию БД
         try:
             db.session.commit()
         except IntegrityError:
             db.session.rollback()
             order_namespace.abort(400, "Ошибка. Невозможно создать заказ")
+
+        user_subscription.quantity -= 1
+
+        try:
+            db.session.commit()  # Сохраняем изменения в подписке пользователя
+        except IntegrityError:
+            db.session.rollback()
+            order_namespace.abort(500, "Ошибка при обновлении подписки пользователя")
+
         return order_schema.dump(order), 201
 
 
